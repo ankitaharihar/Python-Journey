6864d679bbf169cc36a1ec4970876755a5b3d694
Ankita <ankitaharihar81@gmail.com>
2026-06-03 12:20:57 +0530

    Add Python scripts to print a poem and display the multiplication table of 5

---
+++ Chapter 1/Chapter 1 ps/Problem1.py
@@ -0,0 +1,27 @@
+#Write a Python program to print the poem
+
+print(''' Sasa to sasa ki kaapus jasa
+Tyaane kaasawaashi paij laawili
+Wegewege dhaawu ni dongaraawar jaau
+Hi sharyat re apuli
+
+Churuchuru bole to turuturu chaale
+Ni kaasawaane ang halawile
+Sasa jaai pudhe ni jhaadaamaage dade
+Te kaasawaane halu paahiyale
+Waatet thaanbale na, konaashi bolale na
+Chaalale lutulutu paahi sasa 
+
+Hirawi hirawi paane ni paakharaanche gaane
+He paahuniya sasa harakhala
+Khaai gaar chaara, ghe faandicha niwaara
+To haluhalu tethe pengulala
+Mitale wede bhole, gunjeche tyaache dole
+Jhaadaachya saawalit jhope sasa
+
+Jhaali saanj wel to gela kiti kaal
+Ni shahaarali gawataachi paati
+Sasa jhaala jaaga, to uga kari traaga
+Ni dhaaw ghei dongaraachya maathi
+Kaasawa tethe paahi, oshaala mani hoi
+Nijala to snpala, saange sasa ''')
---
+++ Chapter 1/Chapter 1 ps/Problem2.py
@@ -0,0 +1,13 @@
+#Use REPL and print the table of 5 using it.
+#open cmd and type "python"
+
+# 5 * 1
+# 5 * 2
+# 5 * 3
+# 5 * 4
+# 5 * 5
+# 5 * 6
+# 5 * 7
+# 5 * 8
+# 5 * 9
+# 5 * 10
