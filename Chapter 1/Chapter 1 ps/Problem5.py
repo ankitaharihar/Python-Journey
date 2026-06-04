6e8c37d328bf31a9976cb70f5d75a3f99a9124da
Ankita <ankitaharihar81@gmail.com>
2026-06-04 19:09:02 +0530

    Add scripts to list contents of a directory in Chapter 1

--- Chapter 1/Chapter 1 ps/Problem5.py
+++ Chapter 1/Chapter 1 ps/Problem5.py
@@ -1,10 +1,15 @@
+# importing os module
 import os
 
+# path of the directory
 path = "C:\\Users\\Aditya\\OneDrive\\Desktop"
 
+# storing all files and folders in a variable
 files = os.listdir(path)
 
+# printing heading
 print("Contents of directory are:")
 
+# loop to print each file and folder name
 for file in files:
     print(file)
